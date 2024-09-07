465b5754070a775ed580975d2c02750f3a7f76a5
biadmin <virtualkss@snu.ac.kr>
2024-09-07 12:11:58 +0000

    testing show.py

--- show.py
+++ show.py
@@ -1,6 +1,3 @@
-"""
-
-"""
 import gymnasium as gym
 
 from stable_baselines3 import SAC
@@ -40,14 +37,17 @@ def make_env(env_id: str, rank: int, seed: int = 0):
 
 if __name__ == '__main__':
     env_id = 'door_sac'
-    n_cpu = 60
+    n_cpu = 1
     sac_policy = 'MlpPolicy'
     tot_timesteps = 1000000
 
-    vec_env = SubprocVecEnv([make_env(env_id, i, False) for i in range(n_cpu)])
+    vec_env = SubprocVecEnv([make_env(env_id, i) for i in range(n_cpu)])
+    model = SAC.load(env_id, env=vec_env)
 
-    model = SAC(sac_policy, vec_env, train_freq=1, gradient_steps=2, verbose=1, tensorboard_log = f'./{env_id}_tensorboard/')
-    model.learn(total_timesteps=tot_timesteps, log_interval=4, progress_bar = True)
-    model.save(env_id)
-
-    del model
+    obs = vec_env.reset()
+    for t in range(1000):
+    #while True:
+        action, _states = model.predict(obs)
+        #print(action)
+        obs, rewards, dones, info = vec_env.step(action)
+        #vec_env.render()
