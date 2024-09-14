c5c0c84bfd5300510020572359a27d5a5ab72503
biomen <virtualkss@snu.ac.kr>
2024-09-14 11:09:39 +0900

    clean my_show.py

--- my_show.py
+++ my_show.py
@@ -1,71 +1,57 @@
 from stable_baselines3 import SAC
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+import open3d as o3d
+import matplotlib.pyplot as plt
 
 from my_utils import make_env
 
 if __name__ == '__main__':
     env_id = 'door'
+    horizon = 500
     
+    print('making env')
     vec_env = DummyVecEnv([make_env(env_id, 0, True)])
+
+    print('load model')
     model = SAC.load(env_id, env=vec_env)
 
-    print('here 1')
-
+    print('reset env')
     obs = vec_env.reset()
-    for t in range(500):
+
+    for t in range(horizon):
+        print('predict by model')
         action, _states = model.predict(obs)
 
-        print('here 2')
-
-        #from robosuite.utils.camera_utils import grab_next_frame, transform_from_pixels_to_world
-
-        #print(action)
+        print('step forward')
         obs, rewards, dones, info = vec_env.step(action)
         print(t, len(obs), obs)
-        import open3d as o3d
-        print('here 3')
+        
+        print('making rgbd image')
         color_raw = o3d.geometry.Image(vec_env.envs[0].env.env._observables['agentview_image'].obs)
         depth_raw = o3d.geometry.Image(vec_env.envs[0].env.env._observables['agentview_depth'].obs)
         rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw)
         print(rgbd_image)
-        print('here 4')
-
-        import matplotlib.pyplot as plt
-        print('here 5')
+        
+        print('making rgbd image plot')
         plt.subplot(1, 2, 1)
-        plt.title('Redwood grayscale image')
+        plt.title('grayscale image')
         plt.imshow(rgbd_image.color)
         plt.subplot(1, 2, 2)
-        plt.title('Redwood depth image')
+        plt.title('depth image')
         plt.imshow(rgbd_image.depth)
-        print('here 6')
+        print('plot rgbd image')
         plt.show()
-        print('here 7')
+        plt.close()
 
-        o3d.camera.PinholeCameraIntrinsic(
-                o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
-        print('here 8')
-
+        print('making point cloud from rgbd image')
         pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
             rgbd_image,
             o3d.camera.PinholeCameraIntrinsic(
                 o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
         # Flip it, otherwise the pointcloud will be upside down
-        
-        print('here 9')
         pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-        print('here 10')
-        print(type(pcd), pcd)
-        print('here 11')
-        #o3d.visualization.draw_geometries([pcd])
+        print('plot pc by using plotly')
         o3d.visualization.draw_plotly([pcd])
-        print('here 12')
-        #pcd = o3d.io.read_point_cloud("file.pcd", format="pcd")
-        #vis = o3d.visualization.Visualizer()
-        #vis.create_window()
-        #vis.get_render_option().point_color_option = o3d.visualization.PointColorOption.Color
-        #vis.get_render_option().point_size = 3.0
-        #vis.add_geometry(pcd)
-        #o3d.capture_screen_image("vis_test.jpg", do_render=False)
-        print('here 13')
+        
+        print('rendering env')
         vec_env.envs[0].render()
