32af2aaec2057f4446c0b539eaedff9d3e92bba5
biadmin <virtualkss@snu.ac.kr>
2024-10-05 04:59:34 +0000

    experiment_done

--- run_1.sh
+++ run_1.sh
@@ -1,8 +1,17 @@
+# sleep 10
+# python my_train.py --setting candidate --seed 4
+# sleep 30
+# python my_train.py --setting candidate --seed 5
+# sleep 30
+# python my_train.py --setting candidate --seed 6
+# sleep 30
+# python my_train.py --setting candidate --seed 7
+
 sleep 10
-python my_train.py --setting candidate --seed 4
+python my_show.py --setting candidate --seed 4
 sleep 30
-python my_train.py --setting candidate --seed 5
+python my_show.py --setting candidate --seed 5
 sleep 30
-python my_train.py --setting candidate --seed 6
+python my_show.py --setting candidate --seed 6
 sleep 30
-python my_train.py --setting candidate --seed 7
+python my_show.py --setting candidate --seed 7
--- run_3.sh
+++ run_3.sh
@@ -1,9 +1,19 @@
+# sleep 10
+# python my_train.py --setting blind --seed 2
+# sleep 30
+# python my_train.py --setting blind --seed 3
+# sleep 30
+# python my_train.py --setting blind --seed 4
+# sleep 30
+# python my_train.py --setting blind --seed 5
+# sleep 30
+
 sleep 10
-python my_train.py --setting blind --seed 2
+python my_show.py --setting blind --seed 2
 sleep 30
-python my_train.py --setting blind --seed 3
+python my_show.py --setting blind --seed 3
 sleep 30
-python my_train.py --setting blind --seed 4
+python my_show.py --setting blind --seed 4
 sleep 30
-python my_train.py --setting blind --seed 5
+python my_show.py --setting blind --seed 5
 sleep 30
